c6fd0e14a1bf0aae39fb58503081ae6672b8c9e0
Luiz Felipe OP <luiz.felipe221295@hotmail.com>
2022-05-05 17:11:38 -0300

    feat: refactor code

--- src/components/FrontendProjects/index.jsx
+++ src/components/FrontendProjects/index.jsx
@@ -1,72 +1,24 @@
 import { Container } from "./styles";
 
-import kenziebico from "../../assets/bico-com.png";
-import kenzieburguer from "../../assets/kenzie-burguer.png";
-import formulario from "../../assets/signup-form.png";
-import nukenzie from "../../assets/nukenzie.png";
-import studiosmart from "../../assets/studio-smart.png";
-
-import tecnologies from "../../mock/tecnologies";
+import projects from "../../mock/tecnologies";
 
 const FrontendProjects = () => {
   return (
     <Container>
       <ul className="listProjects">
-        <li>
-          <a
-            href="https://bico.vercel.app/"
-            target="blank"
-            role="tooltip"
-            aria-label={"Tecnologias utilizadas:     " + tecnologies[0].message}
-          >
-            <h3>Bico.com</h3>
-            <img src={kenziebico} alt="imagem kenzie bico" />
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://kenzieburguer-wheat.vercel.app/"
-            target="blank"
-            role="tooltip"
-            aria-label={"Tecnologias utilizadas:     " + tecnologies[1].message}
-          >
-            <h3>Kenzie Burguer</h3>
-            <img src={kenzieburguer} alt="imagem kenzie buguer" />
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://formulario-luiz.vercel.app/"
-            target="blank"
-            role="tooltip"
-            aria-label={"Tecnologias utilizadas:     " + tecnologies[2].message}
-          >
-            <h3>Formulário de cadastro</h3>
-            <img src={formulario} alt="imagem formulario de cadastro" />
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://nukenzieluiz.vercel.app/"
-            target="blank"
-            role="tooltip"
-            aria-label={"Tecnologias utilizadas:     " + tecnologies[3].message}
-          >
-            <h3>Nukenzie</h3>
-            <img src={nukenzie} alt="imagem nukenzie" />
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://studio-smart-luiz.vercel.app/"
-            target="blank"
-            role="tooltip"
-            aria-label={"Tecnologias utilizadas:     " + tecnologies[4].message}
-          >
-            <h3>Studio Smart</h3>
-            <img src={studiosmart} alt="imagem studio smart" />
-          </a>
-        </li>
+        {projects.map((project) => (
+          <li>
+            <a
+              href={project.url}
+              target="blank"
+              role="tooltip"
+              aria-label={"Tecnologias utilizadas:     " + project.tecnologies}
+            >
+              <h3>{project.name}</h3>
+              <img src={project.img_url} alt="imagem kenzie bico" />
+            </a>
+          </li>
+        ))}
       </ul>
     </Container>
   );
--- src/mock/tecnologies.js
+++ src/mock/tecnologies.js
@@ -1,22 +1,44 @@
-const tecnologies = [
+import kenziebico from "../assets/bico-com.png";
+import kenzieburguer from "../assets/kenzie-burguer.png";
+import formulario from "../assets/signup-form.png";
+import nukenzie from "../assets/nukenzie.png";
+import studiosmart from "../assets/studio-smart.png";
+
+const projects = [
   {
-    message:
+    name: "Bico.com",
+    url: "https://bico.vercel.app/",
+    img_url: kenziebico,
+    tecnologies:
       "React, Styled Components, React Router DOM, Context API, Framer Motion, JWT, React Rook Form, Yup, React Icons",
   },
   {
-    message: "React, INTL Number Format ",
+    name: "Kenzie Burguer",
+    url: "https://kenzieburguer-wheat.vercel.app/",
+    img_url: kenzieburguer,
+    tecnologies: "React, INTL Number Format ",
   },
   {
-    message: "React, React Router DOM, React Hook Form, Styled Components, Yup",
+    name: "Formulário de cadastro",
+    url: "https://formulario-luiz.vercel.app/",
+    img_url: formulario,
+    tecnologies:
+      "React, React Router DOM, React Hook Form, Styled Components, Yup",
   },
   {
-    message:
+    name: "Nukenzie",
+    url: "https://nukenzieluiz.vercel.app/",
+    img_url: nukenzie,
+    tecnologies:
       "React, React Router DOM, React Icons, Styled Components, INTL Number Format",
   },
   {
-    message:
+    name: "Studio Smart",
+    url: "https://studio-smart-luiz.vercel.app/",
+    img_url: studiosmart,
+    tecnologies:
       "React, React Router DOM, React Icons, Styled Components, UUID, Framer motion, Yup, Axios",
   },
 ];
 
-export default tecnologies;
+export default projects;
